8e2e3ddcbb9c1f65d1aef95c6f8feb515ad27a5e
abdoulraofk <abdoulraofk@gmail.com>
2022-06-18 09:12:48 +0000

    architecture

---
+++ src/app/test/test.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TestPageRoutingModule } from './test-routing.module';
+
+import { TestPage } from './test.page';
+import { ArticleComponent } from '../components/article/article.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TestPageRoutingModule,
+  ],
+  declarations: [TestPage, ArticleComponent]
+})
+export class TestPageModule {}
